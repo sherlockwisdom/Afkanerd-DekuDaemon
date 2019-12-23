9d3653a4aed0fd1b91341f429ccde4a5783352d7
Sherlock Wisdom <wisdomnji@gmail.com>
2019-12-23 20:23:16 +0100

    testing write files

--- tester.cxx
+++ tester.cxx
@@ -12,12 +12,26 @@ string sample_request_string = "number=0000000,message=\"sample request\nmessage
 int main() {
 	CURRENT_SYSTEM_STATE = "TESTING";
 	//CURRENT_SYSTEM_STATE = "PRODUCTION";
-
-	cout << "Testing=> gl_request_queue_listener => " << endl;
-	// input = filename, output = stats
+	cout << "Testing=> write_file => " << endl;
 	helpers::write_file( test_request_file, sample_request_string, ios::trunc, true );
+	vector<string> readfile = helpers::read_file( test_request_file );
+	if( readfile.size() == 1) {
+		cout << "Testing=> Matching number of lines..." << endl;
+		if( readfile[0] == sample_request_string ) {
+			cout << "Testing=> Passed...." << endl;
+		}
+		else {
+			cout << "Testing=> Failed..." << endl;
+			cout << "Request file data seems corrupt..." << endl;
+		}
+	}
+	else {
+		cout << "Testing=> Failed..." << endl;
+		cout << "File has more request than request made..." << endl;
+	}
 
 	//TODO: Build a tester function, not this manual method
+	cout << "Testing=> gl_request_queue_listener => " << endl;
 	if( map<string, string[2] > test_gl_request_queue_listener = gl_request_queue_listener( test_request_file.c_str() ); test_gl_request_queue_listener.size() != helpers::read_file( test_request_file ).size() ) {
 		cout << "Testing=> Failed...." << endl;
 		cout << "processed request = " << test_gl_request_queue_listener.size() <<endl;
