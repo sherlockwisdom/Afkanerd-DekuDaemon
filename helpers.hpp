7a9930f53124670462d2d6f6b8315d4de886dfc7
Sherlock Wisdom <wisdomnji@gmail.com>
2019-12-23 20:16:55 +0100

    fixed issue

--- helpers.hpp
+++ helpers.hpp
@@ -38,8 +38,8 @@ namespace helpers {
 		cout << "[logger_errno] - MESSAGE: " << error_message << "=> " << endl;
 	}
 
-	void write_file( string path_filename, auto input, ios_base::openmode mode = ios::app, bool unescape_string = false ) { //TODO: what about auto
-		if( unescape_string ) input = unescape_string( input );
+	void write_file( string path_filename, auto input, ios_base::openmode mode = ios::app, bool b_unescape_string = false ) { //TODO: what about auto
+		if( b_unescape_string ) input = unescape_string( input );
 		ofstream writefile( path_filename.c_str(), mode );
 		writefile << input;
 		writefile.close();
