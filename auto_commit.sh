32cfab63fc443cc48bea30e4278fcf77a3cfe82b
Sherlock Wisdom <wisdomnji@gmail.com>
2019-12-25 09:48:23 +0100

    fixed autocommit

--- auto_commit.sh
+++ auto_commit.sh
@@ -2,11 +2,11 @@
 
 
 COMMIT_MESSAGE=$1
+
+git add .
+git commit -m "$COMMIT_MESSAGE"
+
 if make
-then
-	git add .
-	git commit -m "$COMMIT_MESSAGE"
 	git push origin master
-else
-	echo "Failed to build... think about branching"
+	echo "Pushed successfully!"
 fi
