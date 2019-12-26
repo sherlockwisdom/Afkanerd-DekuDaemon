38035e6068a572f1a9294276482c6cc7640b0e18
Sherlock Wisdom <wisdomnji@gmail.com>
2019-12-26 21:44:06 +0100

    refactored

--- gl_modem_listener.cxx
+++ gl_modem_listener.cxx
@@ -51,27 +51,7 @@ vector<map<string,string>> gl_modem_listener( ) {
 
 void modem_instance( map<string,string> modem_info ) {
 	//TODO: Ideas, keep this active looking for modem information and shutdown after some tries
-	/*
-	//printf("%s=> modem information... [%s]\n", func_name.c_str(), modem_information[2].c_str());
-	//XXX: Check if another an instance of the modem is already running
-	if(MODEM_DAEMON.find(modem_info["imei"]) != MODEM_DAEMON.end()) {
-		cout << func_name << "=> Instance of Modem already running... watch dog reset!" << endl;
-		std::this_thread::sleep_for(std::chrono::seconds(GL_TR_SLEEP_TIME));
-		return;
-	}
-	if(modem_service_provider.empty()) 
-		modem_service_provider = helpers::split(modem_information[2], ':', true)[1]; //FIXME: What happens when cannot get ISP
-
-	printf("%s=> +ISP[%s] +index[%s] - ", func_name.c_str(), modem_service_provider.c_str(), modem_index.c_str());
-	if(mkdir((char*)(SYS_FOLDER_MODEMS + "/" + modem_imei).c_str(), STD_DIR_MODE) != 0 && errno != EEXIST) {
-		char str_error[256];
-		cerr << "FAILED\n" << func_name << ".error=> " << strerror_r(errno, str_error, 256) << endl;
-	}
-	else {
-		std::thread tr_modem_listener(modem_listener, "\tModem Listener", modem_imei, modem_index, modem_service_provider, true, "MMCLI");
-		tr_modem_listener.detach();
-	}
-	*/
+	
 }
 
 
