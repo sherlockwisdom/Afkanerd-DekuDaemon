733c9ebbc3def4910e828e5db5c9bf3e45ccc708
Sherlock Wisdom <wisdomnji@gmail.com>
2019-12-26 16:23:40 +0100

    cleaned testing file again

--- tester.cxx
+++ tester.cxx
@@ -27,61 +27,6 @@ int main() {
 	SYSTEM_STATE = "DEVELOPMENT";
 	logger::show_state = SYSTEM_STATE;
 
-	Test<size_t> size_t_tester;
-
-	auto list_of_modems = gl_modem_listener();
-	size_t s_list_of_modems = list_of_modems.size();
-	int expected_value = 2;
-	string test_mmcli_index = "1";
-
-	bool test = size_t_tester.equal_values( s_list_of_modems, expected_value );
-	logger::logger_tester("Tester", test);
-	if( !test) {
-		logger::logger("Tester", "\nvalue1: " + to_string( s_list_of_modems ) + "\nvalue2: " + to_string( expected_value ) + "\n");
-	}
-	else {
-		if( list_of_modems[0]["index"] == test_mmcli_index) {
-			logger::logger("Tester", "\nMMCLI Index matches");
-			
-			if( list_of_modems[1]["index"] == "192.168.8.1" ) {
-				logger::logger("Tester", "\nSSH index matches" );
-
-				if( list_of_modems[0]["type"] == "mmcli" ) {
-					logger::logger("Tester", "\nMMCLI type matches" );
-
-					if( list_of_modems[1]["type"] == "ssh" ) {
-						logger::logger("Tester", "\nSSH type matches" );
-
-						if( map<string,string> modem_info = modem_extractor( list_of_modems[0]["index"] ); !modem_info.empty()) {
-							logger::logger("Tester", "\nModem details extracted" );
-							if( modem_info["imei"] == "860016016726544" ) {
-								logger::logger("Tester", "\nMMCLI IMEI Match" );
-							}
-							else {
-								logger::logger("Tester", "\nMMCLI does not match", "stderr");
-							}
-						}
-						else {
-							logger::logger("Tester", "\nModem information could not be extracted\n", "stderr");
-						}
-					}
-					else {
-						logger::logger("Tester", "\nSSH type doesn't match", "stderr");
-					}
-				}
-				else {
-					logger::logger("Tester", "\nMMCLI type doesn't match", "stderr");
-				}
-			}
-			else {
-				logger::logger("Tester", "\nSSH index doesn't match", "stderr");
-			}
-		}
-		else {
-			logger::logger("Tester", "\nMMCLI index doesn't match", "stderr");
-		}
-	}
-
 
 	return 0;
 }
