12a282ad3786b9ad69845b2e2aae40d78a0c6503
Sherlock Wisdom <wisdomnji@gmail.com>
2019-12-26 21:07:35 +0100

    refactored files

--- gl_modem_extractor.cxx
+++ gl_modem_extractor.cxx
@@ -286,14 +286,6 @@ string read_modem_details( string modem_imei ) {
 	return info.empty() ? "" : info[0];
 }
 
-string modem_information_extraction( string arg ) {
-	string func_name = "modem_information_extraction";
-	string ex_command = GET_MODEM_INFO();
-	ex_command = ex_command + " " + arg;
-	logger::logger(func_name, ex_command + "\n");
-
-	return ex_command;
-}
 
 
 map<string,string> modem_extractor( map<string,string> modem_meta_info ) {
@@ -339,7 +331,6 @@ map<string,string> modem_extractor( map<string,string> modem_meta_info ) {
 		if( modem_information.size() > 1 and modem_information[0] == "deku:verified:" ) {
 			modem_meta_info.insert( make_pair( "imei", modem_meta_info["index"] ));
 			modem_meta_info.insert( make_pair( "isp", modem_information[1] ));
-			cout << "ISP: " << modem_information[1] << endl;
 		}
 		else {
 			logger::logger( func_name, "could not verify SSH modem\n", "stderr", true);
@@ -352,42 +343,6 @@ map<string,string> modem_extractor( map<string,string> modem_meta_info ) {
 	}
 
 	return modem_meta_info;
-}
-
-bool is_ssh_modem( string ip ) {
-	return ip.find( GL_SSH_IP_GATEWAY ) != string::npos;
-}
-
-
-string get_modem_type( string modem_index ) {
-	return is_ssh_modem( modem_index ) ? "ssh" : "mmcli";
-}
-
-vector<map<string,string>> gl_modem_listener( ) {
-	string func_name = "gl_modem_listener";
-	vector<map<string,string>> list_of_modems;
-
-	string str_stdout = helpers::terminal_stdout( modem_information_extraction( "list" ));
-	logger::logger( func_name, str_stdout + "\n" );
-
-	if(str_stdout.empty()) {
-		logger::logger(func_name, "No modems found!", "stderr" );
-		return list_of_modems;
-	}
-	else {
-		vector<string> modem_indexes = helpers::split( str_stdout, '\n', true);
-		for( auto& modem_index : modem_indexes ) {
-			modem_index = helpers::remove_char( modem_index, ' ', 'E');
-			map<string,string> modem_information = {
-				{"index", modem_index},
-				{"type", get_modem_type( modem_index )}
-			};
-			
-			list_of_modems.push_back( modem_information );
-		}
-	}
-
-	return list_of_modems;
 }
 
 /*
--- gl_modem_listener.cxx
+++ gl_modem_listener.cxx
@@ -3,8 +3,54 @@
 
 //TODO: if modem becomes disconnected, reiterate to find it... meaning, the check for plug and unplug is here
 
+bool is_ssh_modem( string ip ) {
+	return ip.find( GL_SSH_IP_GATEWAY ) != string::npos;
+}
 
-void modem_listener( map<string,string> modem_info ) {
+
+string get_modem_type( string modem_index ) {
+	return is_ssh_modem( modem_index ) ? "ssh" : "mmcli";
+}
+
+
+string modem_information_extraction( string arg ) {
+	string func_name = "modem_information_extraction";
+	string ex_command = GET_MODEM_INFO();
+	ex_command = ex_command + " " + arg;
+	logger::logger(func_name, ex_command + "\n");
+
+	return ex_command;
+}
+
+vector<map<string,string>> gl_modem_listener( ) {
+	string func_name = "gl_modem_listener";
+	vector<map<string,string>> list_of_modems;
+
+	string str_stdout = helpers::terminal_stdout( modem_information_extraction( "list" ));
+	logger::logger( func_name, str_stdout + "\n" );
+
+	if(str_stdout.empty()) {
+		logger::logger(func_name, "No modems found!", "stderr" );
+		return list_of_modems;
+	}
+	else {
+		vector<string> modem_indexes = helpers::split( str_stdout, '\n', true);
+		for( auto& modem_index : modem_indexes ) {
+			modem_index = helpers::remove_char( modem_index, ' ', 'E');
+			map<string,string> modem_information = {
+				{"index", modem_index},
+				{"type", get_modem_type( modem_index )}
+			};
+			
+			list_of_modems.push_back( modem_information );
+		}
+	}
+
+	return list_of_modems;
+}
+
+void modem_instance( map<string,string> modem_info ) {
+	//TODO: Ideas, keep this active looking for modem information and shutdown after some tries
 	/*
 	//printf("%s=> modem information... [%s]\n", func_name.c_str(), modem_information[2].c_str());
 	//XXX: Check if another an instance of the modem is already running
