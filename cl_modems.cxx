82ad4b3ab14a146c15565bd41f547bab28df47f7
Sherlock Wisdom <wisdomnji@gmail.com>
2019-12-29 12:50:36 +0100

    methods declared and ready to be built into

--- cl_modems.cxx
+++ cl_modems.cxx
@@ -1,9 +1,11 @@
 
 class Jobs {
 	public:
-		string request_a_job() {}
+		string request_a_job();
 
 		map<string,string> extract_jobs();
+
+		void set_isp( string isp );
 }; 
 
 class Modems {
@@ -31,3 +33,21 @@ class Modem : public Modems, Jobs {
 
 		bool send_sms( string message, string number );
 };
+
+
+string Jobs::request_a_job( ) {}
+map<string,string> Jobs::extract_jobs() {}
+void Jobs::set_isp( string isp ) {}
+
+
+
+bool Modems::is_available() {}
+string Modems::get_modem_type() {}
+
+bool Modems::is_ssh_modem( string index ) {}
+bool Modems::run_daemon_listener() {}
+
+string Modems::modem_information_extraction( string arg ) {}
+
+void Modem::start_modem_instance() {}
+bool Modem::send_sms( string message, string number ) {}
