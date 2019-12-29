d1aa70ababb07e451a4087f8172cdeb5376d660b
Sherlock Wisdom <wisdomnji@gmail.com>
2019-12-29 12:44:44 +0100

    can try to make functions happen now

--- cl_modems.cxx
+++ cl_modems.cxx
@@ -1,21 +1,24 @@
 
-class DekuFunctions {
+class Jobs {
 	public:
 		string request_a_job() {}
 
 		map<string,string> extract_jobs();
-
 }; 
 
 class Modems {
+	protected: 
+		string modem_information_extraction( string arg );
 	private:
 		void run_daemon_listener();
+		bool is_ssh_modem( string index );
 	public:
 		bool is_available( string imei );
+		string get_modem_type( string index );
 
 };
 
-class Modem {
+class Modem : public Modems, Jobs {
 	private:
 		string type;
 		string index;
