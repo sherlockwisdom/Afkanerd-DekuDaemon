70ff383f1c0c751f5dc5f2defb6216622335368b
Sherlock Wisdom <wisdomnji@gmail.com>
2020-01-23 08:41:33 +0100

    wiring new parsers

---
+++ src/parsers/comma_equal_parser_test.cxx
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "comma_equals_parser.hpp"
+
+using namespace std;
+
+
+int main() {
+	cout << __FUNCTION__ << ":Testing: comma_seperate" << endl;
+	cout << __FUNCTION__ << ":Testing: equal_seperate" << endl;
+
+
+	return 0;
+}
---
+++ src/parsers/comma_equals_parser.hpp
@@ -0,0 +1,13 @@
+#include <vector>
+#ifndef COMMA_EQUAL_PARSER_H_INCLUDED_
+#define COMMA_EQUAL_PARSER_H_INCLUDED_
+
+using namespace std;
+
+namespace parsers {
+	vector<string> comma_seperate( string input ) {}
+
+	vector<string> equal_seperate( string input ) {}
+}
+
+#endif
