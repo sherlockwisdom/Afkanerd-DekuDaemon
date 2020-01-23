c45da25fff0d1dd188b3e3430ac60ed7c8ff0d9c
Sherlock Wisdom <wisdomnji@gmail.com>
2020-01-23 10:00:52 +0100

    comma seperate value passed

--- src/parsers/comma_equal_parser_test.cxx
+++ src/parsers/comma_equal_parser_test.cxx
@@ -2,10 +2,23 @@
 #include "comma_equals_parser.hpp"
 
 using namespace std;
-
+//TODO: Allow parser to choose parsing or ignoring double quotes
 
 int main() {
+	string sample = "number=00000,message=Hello world\"this is a sample message,retry=true";
+	vector<string> comma_expected_sample = {"number=00000", "message=Hello world\"this is a sample message", "retry=true"};
+	vector<string> parser_sample = parsers::comma_seperate( sample );
+
 	cout << __FUNCTION__ << ":Testing: comma_seperate" << endl;
+	if( comma_expected_sample == parser_sample ) {
+		cout << __FUNCTION__  << ": Passed" << endl;
+	}
+	else {
+		cout << __FUNCTION__ << ": Failed" << endl;
+		for(auto i : parser_sample ) cout << i << ", ";
+		cout << endl;
+	}
+	cout << endl;
 	cout << __FUNCTION__ << ":Testing: equal_seperate" << endl;
 
 
