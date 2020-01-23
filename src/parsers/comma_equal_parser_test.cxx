43ccc566e6d71f6c997c802f11e61271801432f9
Sherlock Wisdom <wisdomnji@gmail.com>
2020-01-23 10:56:45 +0100

    figured out how shit works in makefiles :D lol

--- src/parsers/comma_equal_parser_test.cxx
+++ src/parsers/comma_equal_parser_test.cxx
@@ -8,13 +8,15 @@ int main() {
 	string sample = "number=00000,message=Hello world\"this is a sample message,retry=true";
 	vector<string> comma_expected_sample = {"number=00000", "message=Hello world\"this is a sample message", "retry=true"};
 	vector<string> equal_expected_sample = {"number", "00000,message", "Hello world\"this is a sample message,retry", "true"};
-	vector<string> in_delimeter_expected_sample = {"message=Hello world\"this is a sample message"};
+	vector<string> in_delimeter_expected_comma_sample = {"message=Hello world\"this is a sample message"};
+	vector<string> in_delimeter_expected_equal_sample = {"00000,message"};
 
 	vector<string> parser_sample = parsers::comma_seperate( sample );
 	vector<string> parser_sample1 = parsers::equal_seperate( sample );
 	vector<string> parser_sample2 = parsers::general_seperate( sample, ',');
 	vector<string> parser_sample21 = parsers::general_seperate( sample, '=');
 	vector<string> parser_sample3 = parsers::in_delimeter_extract( sample, ',');
+	vector<string> parser_sample31 = parsers::in_delimeter_extract( sample, '=');
 
 	cout << __FUNCTION__ << ":Testing: comma_seperate" << endl;
 	if( comma_expected_sample == parser_sample ) {
@@ -57,12 +59,22 @@ int main() {
 
 	cout << endl;
 	cout << __FUNCTION__ << ":Testing: in_delimeter_seperate" << endl;
-	if( in_delimeter_expected_sample == parser_sample3 ) {
-		cout << __FUNCTION__ << ": PASSED" << endl;
+	if( in_delimeter_expected_comma_sample == parser_sample3 ) {
+		cout << __FUNCTION__ << ": comma PASSED" << endl;
 	}
 	else {
-		cout << __FUNCTION__ << ": FAILED" << endl;
+		cout << __FUNCTION__ << ": comma FAILED" << endl;
 		for(auto i : parser_sample3 ) cout << i << ", ";
+	}
+
+	cout << endl;
+	cout << __FUNCTION__ << ":Testing: in_delimeter_seperate" << endl;
+	if( in_delimeter_expected_equal_sample == parser_sample31 ) {
+		cout << __FUNCTION__ << ": equal PASSED" << endl;
+	}
+	else {
+		cout << __FUNCTION__ << ": equal FAILED" << endl;
+		for(auto i : parser_sample31 ) cout << i << ", ";
 	}
 	return 0;
 }
