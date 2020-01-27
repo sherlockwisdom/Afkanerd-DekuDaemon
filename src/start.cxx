330107565dea2cf55f8948c6059a29329670e310
Sherlock Wisdom <wisdomnji@gmail.com>
2020-01-27 11:38:26 +0100

    added some checks

--- src/start.cxx
+++ src/start.cxx
@@ -8,6 +8,26 @@ std::string PATH_SYS_FILE = "build_files/sys_file.txt";
 
 bool system_check( string path_to_sys_file) {
 	logger::logger( __FUNCTION__, "Running system check at: " + path_to_sys_file, "stdout", true);
+
+	if( !helpers::file_exist(path_to_sys_file)) {
+		logger::logger(__FUNCTION__, "System file does not exist", "stderr", true);
+		return false;
+	}
+
+	vector<string> sys_file_contents = helpers::read_file( path_to_sys_file );
+	if( sys_file_contents.size() < 1 ) {
+		//TODO: put logger statement
+	}
+
+	for(auto config_line: sys_file_contents) {
+		vector<string> configs = helpers::split(config_line, ':');
+		if(configs[0] == "DIR_REQUEST_FILE") {
+			//TODO: check if dir exist, else create it
+		}
+		else if(configs[0] == "DIR_ISP") {
+			//TODO: check if dir exist, else create it
+		}
+	}
 }
 
 
