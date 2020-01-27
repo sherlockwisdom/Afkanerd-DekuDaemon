8d5b1647272ed808067f630d6473166545436a9a
Sherlock Wisdom <wisdomnji@gmail.com>
2020-01-27 18:03:58 +0100

    added some code that works, but then does nothing

--- src/inotify.cxx
+++ src/inotify.cxx
@@ -1,5 +1,6 @@
 #include <sys/inotify.h>
 #include <iostream>
+#include "formatters/logger.hpp"
 
 using namespace std;
 
@@ -11,5 +12,12 @@ int main() {
 		return 1;
 	}
 
+	int i_watch = inotify_add_watch(i_init, "inotify.cxx", IN_MODIFY);
+	if(i_watch == -1) {
+		cerr << "Failed to watch file" << endl;
+		logger::logger_errno( errno );
+		return 1;
+	}
 
+	return 0;
 }
