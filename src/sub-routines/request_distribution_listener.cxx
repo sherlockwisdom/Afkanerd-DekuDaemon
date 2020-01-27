ae956dc2ce70caea91efa6c73e6767d66109bd0c
Sherlock Wisdom <wisdomnji@gmail.com>
2020-01-27 12:40:25 +0100

    request distribution creation

---
+++ src/sub-routines/request_distribution_listener.cxx
@@ -0,0 +1,15 @@
+//TODO: Keeps event listener for changes to request file
+//TODO: Parses request file
+//TODO: send parsed content to isp distributor
+
+
+#include "request_distribution_listener.hpp"
+
+using namespace std;
+
+
+void request_distribution_listener( map<string, string> configs ) {
+
+}
+
+
