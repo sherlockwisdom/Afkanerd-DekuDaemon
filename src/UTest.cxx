af198b62f13fda40fb0dcbe13104c636b948f9c1
Sherlock Wisdom <wisdomnji@gmail.com>
2020-01-27 21:07:19 +0100

    Good night for today

--- src/UTest.cxx
+++ src/UTest.cxx
@@ -5,5 +5,6 @@ using namespace std;
 int main() {
 	cout << boolalpha << helpers::file_exist("formatters/") << endl;
 	cout << boolalpha << helpers::file_exist("UTest.cxx") << endl;
+	cout << boolalpha << helpers::file_exist("UTest_na.cxx") << endl;
 	return 0;
 }
--- src/sub-routines/request_distribution_listener.cxx
+++ src/sub-routines/request_distribution_listener.cxx
@@ -36,13 +36,19 @@ void request_distribution_listener( map<string, string> configs ) {
 				for(auto request : requests) {
 					vector<string> request_extract = parsers::comma_seperation( request );
 					for(auto r_entity : request_extract ) {
+						//TODO: critical... if equals is in message, it will parse through the message
 						vector<string> component = parsers::equal_seperate( r_entity );
 						if( component[0] == "number" ) number = component[1];
 						else if(component[0] == "message" ) message = component[1];
 					}
 					string isp = isp_determiner::get_isp( number );
-					//TODO: check if ISP folder exist....
+					//TODO: check if ISP folder exist...
+					if( !helpers::file_exist( config["DIR_ISP"] + "/" + isp + "/" ) ) {
+						helpers::make_dir(config["DIR_ISP"] + "/" + isp);
+					}
+					
 				}
+			}
 			//TODO: send new file to isp distributor
 			//TODO: isp distributor parses file and share among isp folders
 			//TODO: sends confirmation message and begins again
