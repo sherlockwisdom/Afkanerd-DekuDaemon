7401a6368a14281c28bf3c8205414c2150662403
Sherlock Wisdom <wisdomnji@gmail.com>
2020-01-27 10:44:58 +0100

    test added and test passed

--- src/sub-routines/isp_determiner_test.cxx
+++ src/sub-routines/isp_determiner_test.cxx
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include "isp_determiner.hpp"
 
 using namespace std;
@@ -7,3 +8,30 @@ int main() {
 	vector<string> mtn_numbers = {"652000000", "671000000", "673000000", "680000000", "672000000"};
 	vector<string> orange_numbers = {"691000000", "699000000", "6580000000", "6940000000", "698000000"};
 	vector<string> nextel_numbers = {"667000000"};
+
+
+	cout << __FUNCTION__ << ": Testing MTN numbers: " << endl;
+	for(auto number : mtn_numbers) {
+		if( isp_determiner::get_isp( number) != "MTN" ) {
+			cout << "FAILED" << endl;
+			cout << number << " is not an MTN number" << endl;
+		}
+	}
+	cout << __FUNCTION__ << ": Testing Orange Numbers: " << endl;
+	for(auto number : mtn_numbers) {
+		if( isp_determiner::get_isp( number) != "Orange" ) {
+			cout << "FAILED" << endl;
+			cout << number << " is not an Orange number" << endl;
+		}
+	}
+	cout << __FUNCTION__ << ": Testing Nexttell numbers: " << endl;
+	for(auto number : mtn_numbers) {
+		if( isp_determiner::get_isp( number) != "Nexttell" ) {
+			cout << "FAILED" << endl;
+			cout << number << " is not an Nexttell number" << endl;
+		}
+	}
+
+	return 0;
+}
+
