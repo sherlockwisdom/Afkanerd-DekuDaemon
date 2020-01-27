4ff97902c5ff46d935a241e212d1fbe05a2cc902
Sherlock Wisdom <wisdomnji@gmail.com>
2020-01-27 10:37:32 +0100

    added numbers for testing

---
+++ src/sub-routines/isp_determiner.hpp
@@ -0,0 +1,11 @@
+#ifndef ISP_DETERMINER_H_INCLUDE_
+#define ISP_DETERMINER_H_INCLUDE_
+using namespace std;
+
+namespace isp_determiner {
+	string get_isp( string number ) {
+	}
+}
+
+
+#endif
---
+++ src/sub-routines/isp_determiner_test.cxx
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "isp_determiner.hpp"
+
+using namespace std;
+
+int main() {
+	vector<string> mtn_numbers = {"652156811", "671932990", "673095628", "680946031", "672560060"};
+	vector<string> orange_numbers = {"691979004", "699282830", "658470379", "694629225", "698665381"};
+	vector<string> nextel_numbers = {"667737750"};
