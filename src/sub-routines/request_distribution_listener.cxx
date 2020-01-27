357d9c38cd6e04425720d057cde9426e80a7dc1b
Sherlock Wisdom <wisdomnji@gmail.com>
2020-01-27 12:51:02 +0100

    added more request file functions

--- src/sub-routines/request_distribution_listener.cxx
+++ src/sub-routines/request_distribution_listener.cxx
@@ -8,8 +8,18 @@
 using namespace std;
 
 
-void request_distribution_listener( map<string, string> configs ) {
+bool configs_check( map<string,string> configs ) {
+	//TODO: put the things one needs to check in here
+}
 
+void request_distribution_listener( map<string, string> configs ) {
+	//TODO: run system checks here
+	if( !configs_check(configs)) {
+		logger::logger( __FUNCTION__, "Configuration check failed, cannot start", "stderr", true);
+		return;
+	}
+
+	PATH_REQUEST_FILE = configs["DIR_REQUEST_FILE"] + "/REQUEST_FILE.txt";
 }
 
 
