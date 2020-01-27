6febc619b7fe04c348437a0da59f0d77e3e5c8a9
Sherlock Wisdom <wisdomnji@gmail.com>
2020-01-27 10:52:55 +0100

    added isp_checks

--- src/sub-routines/isp_determiner.hpp
+++ src/sub-routines/isp_determiner.hpp
@@ -4,7 +4,51 @@ using namespace std;
 
 namespace isp_determiner {
 	string get_isp( string number ) {
-	}
+		if(number[0] == '6') {
+			switch(number[1]) {
+				case '5':
+					switch(number[2]) {
+						case '0':
+						case '1':
+						case '2':
+						case '3':
+						case '4':
+							return "MTN";
+						break;
+
+						case '5':
+						case '6':
+						case '7':
+						case '8':
+						case '9':
+							return "ORANGE";
+						break;
+
+					}
+				break;
+
+				case '6': return "NEXTTEL";
+
+				case '7': return "MTN";
+				break;
+
+				case '8':
+					  switch(number[2]) {
+						case '0':
+						case '1':
+						case '2':
+						case '3':
+						case '4':
+							return "MTN";
+						break;
+					  }
+				break;
+
+				case '9': return "ORANGE";
+				break;
+			}
+		}
+		return "unknown";
 }
 
 
