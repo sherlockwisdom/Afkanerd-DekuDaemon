7b98e8e670ef82d10ff6937265f8a1c0594e836f
Sherlock Wisdom <wisdomnji@gmail.com>
2020-01-28 20:12:25 +0100

    should be working, cus it compiles and runs

--- src/sub-routines/request_distribution_listener_test.cxx
+++ src/sub-routines/request_distribution_listener_test.cxx
@@ -3,5 +3,20 @@
 using namespace std;
 
 int main() {
+	map<string,string> configs{
+		{"DIR_ISP", "test_dir"},
+		{"DIR_REQUEST_FILE", "test_dir"},
+		{"STD_NAME_REQUEST_FILE", "request_file.txt"}
+	};
 
+	if(!configs_check( configs ) ) {
+		cout << "Configs check Failed..." << endl;
+	}
+
+	string message = "Hello world\\nNew line";
+	string number = "67000";
+
+	if( isp_distributor( message, number, configs ) != "MTN" ) {
+		cout << "ISP distribution Failed..." << endl;
+	}
 }
