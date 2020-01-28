71156c3d1306a18e6f9f9071ba4edb8ef8bde91a
Sherlock Wisdom <wisdomnji@gmail.com>
2020-01-28 20:59:38 +0100

    made the test better

--- src/start_test.cxx
+++ src/start_test.cxx
@@ -4,6 +4,11 @@ using namespace std;
 
 int main() {
 	string sys_config_file = "test_dir/test_config.txt";
+	if( system_check( sys_config_file ) ) cout << "System file check passed" << endl;
+	else cout << "System file check failed" << endl;
+
+
+	vector<string> file_contents = helpers::read_file( sys_config_file );
 
 	map<string,string> sample_config_file = get_system_configs( file_contents );
 
@@ -12,10 +17,10 @@ int main() {
 			sample_config_file.find("STD_NAME_REQUEST_FILE") != sample_config_file.end() and
 			sample_config_file.find("DIR_REQUEST_FILE") != sample_config_file.end()
 	  ) {
-		cout << "Passed..." << endl;
+		cout << "Get system configs Passed..." << endl;
 	}
 	else {
-		cout << "Failed..." << endl;
+		cout << "Get system configs Failed..." << endl;
 	}
 
 	return 0;
