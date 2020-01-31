206ff72a5145f95582358258e6fd181049a65d23
Sherlock Wisdom <wisdomnji@gmail.com>
2020-01-31 18:45:00 +0100

    worked on makefile

--- src/sub-routines/modem_listener_test.cxx
+++ src/sub-routines/modem_listener_test.cxx
@@ -4,10 +4,12 @@ using namespace std;
 
 
 int main() {
+	map<string, string> list_of_test_modems;
+	vector<string> list_of_test_modems_isp;
+	vector<string> list_of_test_modems_imei;
 	Modems modems;
 
 	modems = modems.getAll();
-	
 	if(modems.getAllIndexes().size() == list_of_test_modems.size()) {
 	}
 
