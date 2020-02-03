420fc74c4e0752113edb9c821e814bc236ba1da3
Sherlock Wisdom <wisdomnji@gmail.com>
2020-02-03 14:14:34 +0100

    found and fixed the problem

--- src/sub-routines/modem_listener.cxx
+++ src/sub-routines/modem_listener.cxx
@@ -110,15 +110,17 @@ bool Modems::start( Modem modem ) {
 
 void Modems::startAllModems() {
 	while( 1 ) {  //TODO: Change this to a condition
-		logger::logger(__FUNCTION__, "Looking to start new modems...");
+		logger::logger(__FUNCTION__, to_string(this->threaded_modems.size()) + " currently threaded modems");
 		for(map<Modem*, std::thread>::iterator it=this->threaded_modems.begin();it!=this->threaded_modems.end();++it ) {
 			if(std::find(this->modemCollection.begin(), this->modemCollection.end(), it->first) == this->modemCollection.end()) { //This checks against mem values
 			//if(std::find_if(this->modemCollection.begin(), this->modemCollection.end(), [&](Modem* modem1){ return *modem1 == modem; }) == this->modemCollection.end()) { //This checks against type of modem
 				logger::logger(__FUNCTION__, it->first->getInfo() + " - Modem not available, stopping thread");
 				if(this->threaded_modems.find(it->first) != this->threaded_modems.end()) {
-					it->first->end();
-					while(!it->first->getThreadSafety()) helpers::sleep_thread(5);
-					this->threaded_modems.erase(it);
+					//it->first->end();
+					//while(!it->first->getThreadSafety()) helpers::sleep_thread(5);
+					//this->threaded_modems.erase(it);
+					
+					logger::logger(__FUNCTION__, to_string(this->threaded_modems.size()) + " currently threaded modems");
 				}
 			}
 		}
