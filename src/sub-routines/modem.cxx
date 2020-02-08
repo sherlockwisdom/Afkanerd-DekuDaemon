0b5a4b82014af51ed5617a5773af4c8f9b3d0d95
Sherlock Wisdom <wisdomnji@gmail.com>
2020-02-08 22:38:11 +0100

    file is now moved to successfull

--- src/sub-routines/modem.cxx
+++ src/sub-routines/modem.cxx
@@ -146,7 +146,7 @@ void modem_request_listener( Modem* modem ) {
 					cout << "U_Filename: " << request["u_filename"] << endl;
 					cout << "Q_Filename: " << request["q_filename"] << endl;
 
-					if(string locked_filename = request["filename"]; !sys_calls::rename_file(locked_filename, modem->getConfigs()["DIR_SUCCESS"] + "/" + locked_filename) and !sys_calls::rename_file(modem->getConfigs()["DIR_SUCCESS"] + "/." + request["q_filename"], modem->getConfigs()["DIR_SUCCESS"] + "/" + request["q_filename"])) {
+					if(string locked_filename = request["filename"]; !sys_calls::rename_file(locked_filename, modem->getConfigs()["DIR_SUCCESS"] + "/" + request["q_filename"]) and !sys_calls::rename_file(modem->getConfigs()["DIR_SUCCESS"] + "/." + request["q_filename"], modem->getConfigs()["DIR_SUCCESS"] + "/" + request["q_filename"])) {
 						logger::logger(__FUNCTION__, modem->getInfo() + " - Failed to move file to DIR_SUCCESS", "stderr", true);
 						logger::logger_errno( errno );
 					}
