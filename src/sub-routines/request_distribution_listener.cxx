3cc1ef926875ce2804997d5f5c22b12b5ba8fa95
Sherlock Wisdom <wisdomnji@gmail.com>
2020-02-08 20:35:57 +0100

    added ID to the entire thing

--- src/sub-routines/request_distribution_listener.cxx
+++ src/sub-routines/request_distribution_listener.cxx
@@ -15,9 +15,9 @@ namespace request_distribution_listener {
 		);
 	}
 
-	string isp_distributor( string message, string number, map<string,string> config ) {
+	string isp_distributor( string message, string number, string id, map<string,string> config ) {
 		string isp = isp_determiner::get_isp( number );
-		string request = "number="+number+",message=\""+message+"\"";
+		string request = "id="+id+",number="+number+",message=\""+message+"\"";
 		logger::logger(__FUNCTION__, "Distributing [" + request + "] - [" + isp + "]");
 		if( !helpers::file_exist( config["DIR_ISP"] + "/" + isp + "/" ) ) {
 			helpers::make_dir(config["DIR_ISP"] + "/" + isp);
@@ -32,6 +32,7 @@ namespace request_distribution_listener {
 		string message, number;
 		for(auto r_entity : request_extract ) {
 			vector<string> component = parsers::equal_seperate( r_entity, 1);
+			if(component.size() < 2 ) continue;
 			if( component[0] == "number" ) extracted_request.insert(make_pair("number", component[1]));
 			else if(component[0] == "message" ) {
 				if(component[1][0] == '"') component[1].erase(0,1);
@@ -39,6 +40,7 @@ namespace request_distribution_listener {
 				message = component[1];
 				extracted_request.insert(make_pair("message", message));
 			}
+			else if(component[0] == "id") extracted_request.insert(make_pair("id", component[1]));
 		}
 
 		return extracted_request;
@@ -65,7 +67,8 @@ namespace request_distribution_listener {
 						map<string,string> extracted_request = request_parser( request );
 						string number = extracted_request["number"];
 						string message = extracted_request["message"];
-						isp_distributor( message, number, configs );
+						string id = extracted_request["id"];
+						isp_distributor( message, number, id, configs );
 					}
 					if(!sys_calls::file_handlers( random_name, sys_calls::DEL )) {
 						logger::logger(__FUNCTION__, "Failed to delete old file, not bad but not good...", "stderr", true);
