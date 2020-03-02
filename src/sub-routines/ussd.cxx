1612e783abd629f9be9dc95ad7611e9b59d124f1
Sherlock Wisdom <wisdomnji@gmail.com>
2020-03-02 19:24:21 +0100

    overloaded initiate_series function

--- src/sub-routines/ussd.cxx
+++ src/sub-routines/ussd.cxx
@@ -38,6 +38,33 @@ multimap<string,string> USSD::initiate_series( vector<string> commands ) {
 	return responses;
 }
 
+multimap<string,string> USSD::initiate_series( vector<string> args, vector<string> commands ) {
+	multimap<string, string> responses;
+
+	if( args.empty()) return responses;
+	if( commands.empty() ) return responses;
+
+	size_t args_counter = 0;
+	for( auto& command : commands ) {
+		if( command == "%%" ) {
+			command = args[args_counter];
+			++args_counter;
+		}
+	}
+	
+	string terminal_response = this->initiate( commands[0] );
+	if( terminal_response.empty()) return responses;
+
+	responses.insert(make_pair(commands[0], terminal_response));
+	for(auto command = commands.begin() + 1; command != commands.end(); ++command) {
+		terminal_response = this->respond( *command );
+		if( terminal_response.empty()) break;
+		responses.insert(make_pair(*command, terminal_response));
+	}
+
+	return responses;
+}
+
 string USSD::respond( string command ) {
 	string terminal_request = this->configs["DIR_SCRIPTS"] + "/modem_information_extraction.sh ussd_respond " + this->modem_index + " " + command;
 	//logger::logger(__FUNCTION__, terminal_request );
