5def4f70298c11abe201c8ee0345ba662269b509
Sherlock Wisdom <wisdomnji@gmail.com>
2020-03-16 21:26:50 +0100

    everything extracts and everything shows

--- src/sub-routines/modem_test.cxx
+++ src/sub-routines/modem_test.cxx
@@ -9,5 +9,12 @@ int main() {
 
 	vector<map<string,string>> sms_messages = modem.get_sms_messages();
 
+	for(auto sms_message : sms_messages) {
+		for(auto component : sms_message) {
+			logger::logger(__FUNCTION__, component.first + " -> " + component.second);
+		}
+		cout << endl;
+	}
+
 	return 0;
 }
