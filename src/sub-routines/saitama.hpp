49e64d7218b9c989081357876d2845aa4085230a
Sherlock Wisdom <wisdomnji@gmail.com>
2020-03-17 14:39:30 +0100

    added the saitama script

---
+++ src/sub-routines/saitama.hpp
@@ -0,0 +1,13 @@
+#ifndef SAITAMA_H_INCLUDED_
+#define SAITAMA_H_INCLUDED_
+
+using namespace std;
+
+namespace saitama {
+
+	map<string,string> executions {
+		{"--:all_might:--", ""}
+	};
+}
+
+#endif
