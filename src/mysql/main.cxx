b83c720a18e9d369821271a6f48ef0781e4e95e3
Sherlock Wisdom <wisdomnji@gmail.com>
2020-03-20 17:34:15 +0100

    can get the mysql to return values

--- src/mysql/main.cxx
+++ src/mysql/main.cxx
@@ -1,7 +1,27 @@
+#include <iostream>
 #include <mysql.h>
 
 using namespace std;
 
 int main() {
 	
+	MYSQL *mysqlConnection;
+	MYSQL_RES *mysqlResult;
+	MYSQL_ROW mysqlRow;
+
+	string server = "localhost";
+	string user = "root";
+	string password = "asshole";
+	string database = "__DEKU__";
+
+
+	mysqlConnection = mysql_init( NULL );
+
+	auto mysql_connection_state_good = mysql_real_connect( mysqlConnection, server.c_str(), user.c_str(), password.c_str(), database.c_str(), 0, NULL, 0);
+	auto mysql_connection_state_bad = mysql_real_connect( mysqlConnection, "localhost_1", user.c_str(), password.c_str(), database.c_str(), 0, NULL, 0 );
+
+	cout << "Good state: "  << mysql_connection_state_good << endl;
+	cout << "Bad state: " << mysql_connection_state_bad << endl;
+
+	return 0;
 }
