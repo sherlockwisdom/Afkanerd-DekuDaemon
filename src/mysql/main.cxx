b264040fc81d806d406b8e502e3de299342a22ae
Sherlock Wisdom <wisdomnji@gmail.com>
2020-03-20 17:41:09 +0100

    mysql be looking so good!

--- src/mysql/main.cxx
+++ src/mysql/main.cxx
@@ -9,7 +9,7 @@ int main() {
 	MYSQL_RES *mysqlResult;
 	MYSQL_ROW mysqlRow;
 
-	string server = "localhost";
+	string server = "localhost_1";
 	string user = "root";
 	string password = "asshole";
 	string database = "__DEKU__";
@@ -18,10 +18,10 @@ int main() {
 	mysqlConnection = mysql_init( NULL );
 
 	auto mysql_connection_state_good = mysql_real_connect( mysqlConnection, server.c_str(), user.c_str(), password.c_str(), database.c_str(), 0, NULL, 0);
-	auto mysql_connection_state_bad = mysql_real_connect( mysqlConnection, "localhost_1", user.c_str(), password.c_str(), database.c_str(), 0, NULL, 0 );
 
-	cout << "Good state: "  << mysql_connection_state_good << endl;
-	cout << "Bad state: " << mysql_connection_state_bad << endl;
+	if( !mysql_connection_state_good ) {
+		cerr << "Failed to connect to database: " << mysql_error( mysqlConnection ) << endl;
+	}
 
 	return 0;
 }
