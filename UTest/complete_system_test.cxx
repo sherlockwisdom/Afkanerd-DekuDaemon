da4ed9db959f7220d241f98c2cde376b54a30339
Sherlock Wisdom <wisdomnji@gmail.com>
2020-04-05 23:32:29 +0100

    first test created and succeeded, moral of the story, put custom headers first before cpputest headers

--- UTest/complete_system_test.cxx
+++ UTest/complete_system_test.cxx
@@ -1,14 +1,24 @@
+// Custom header files
+#include "../src/formatters/helpers.hpp"
+
 #include "CppUTest/TestHarness_c.h"
 #include "CppUTest/CommandLineTestRunner.h"
+// #include "CppUTest/MockSupport.h"
 
 
+TEST_GROUP(Helpers) {};
 
+TEST(Helpers, remove_char) {
+	std::string input_string = "hello;;world";
+	std::string expected_string = "helloworld";
+	std::string output_string = helpers::remove_char( input_string, ';');
 
-
-
-
+	// (expected, actual)
+	STRCMP_EQUAL(expected_string.c_str(), output_string.c_str()); 
+}
 
 int main( int argc, char** argv ) {
-
+	// Testing to see how the recording works in this aspect
+	return CommandLineTestRunner::RunAllTests(argc, argv);
 }
 
