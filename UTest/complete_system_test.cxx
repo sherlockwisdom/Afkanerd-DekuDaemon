200eea1638afe0576f091f530e6a2a4919c1b1a9
Sherlock Wisdom <wisdomnji@gmail.com>
2020-04-06 10:49:49 +0100

    templates are stupid, but the whole type is deduced right at the start of the program

--- UTest/complete_system_test.cxx
+++ UTest/complete_system_test.cxx
@@ -33,10 +33,21 @@ TEST(Helpers, vector_to_whole_string) {
 	char seperation_del = ' ';
 	std::vector<std::string> input_vector{"hello","world"};
 	std::string expected_string = "hello world";
-	std::string output_string = helpers::vector_to_whole_x<string>( input_vector,seperation_del);
+	std::string output_string = helpers::vector_to_whole_string<string>( input_vector,seperation_del);
 
 	// (expected, actual)
 	STRCMP_EQUAL(expected_string.c_str(), output_string.c_str()); 
+}
+
+// Testing: helpers::vector_to_whole_x( template vector, char = ' ')
+TEST(Helpers, vector_to_whole_numbers) {
+	//char seperation_del = ' ';
+	std::vector<int> input_vector{1,2};
+	int expected_int = 12;
+	int output_int = helpers::vector_to_whole_numbers<int>( input_vector);
+
+	// (expected, actual)
+	LONGS_EQUAL(expected_int, output_int); 
 }
 
 int main( int argc, char** argv ) {
