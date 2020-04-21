3c1ebdb7b269aaf9217e1115d874b456f9bf5c6e
Sherlock Wisdom <wisdomnji@gmail.com>
2020-04-21 15:58:17 +0100

    haven't tested the code yet, but it should work

--- UTest/parsers.cxx
+++ UTest/parsers.cxx
@@ -4,7 +4,16 @@
 #include "CppUTest/TestHarness_c.h"
 #include "CppUTest/CommandLineTestRunner.h"
 
-TEST_GROUP(Helpers) {};
+TEST_GROUP(Parsers) {};
+
+TEST(Parsers, comma_seperate) {
+	std::string input_string = "hello,world,";
+	std::vector<std::string> expected_output {"hello", "world"};
+	std::vector<std::string> output_vector = parsers::comma_seperate( input_string, ';');
+
+	// (expected, actual)
+	STRCMP_EQUAL(expected_string.c_str(), output_string.c_str()); 
+}
 
 
 int main( int argc, char** argv ) {
