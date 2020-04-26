7125cf55f2a3a835a35eeb731f105d1d228c9789
Sherlock Wisdom <wisdomnji@gmail.com>
2020-04-26 21:50:16 +0100

    added getters for mysql

--- UTest/mysql_test.cxx
+++ UTest/mysql_test.cxx
@@ -14,10 +14,10 @@ TEST(Mysql, Constructor) {
 
 	MySQL mysql(mysqlServer, mysqlUser, mysqlPassword, mysqlDatabase);
 
-	STRCMP_EQUAL( mysqlServer.c_str(), mysql->get_server().c_str());
-	STRCMP_EQUAL( mysqlUser.c_str(), mysql->get_user().c_str());
-	STRCMP_EQUAL( mysqlPassword.c_str(), mysql->get_password().c_str());
-	STRCMP_EQUAL( mysqlDatabase.c_str(), mysql->get_database().c_str());
+	STRCMP_EQUAL( mysqlServer.c_str(), mysql.get_server().c_str());
+	STRCMP_EQUAL( mysqlUser.c_str(), mysql.get_user().c_str());
+	STRCMP_EQUAL( mysqlPassword.c_str(), mysql.get_password().c_str());
+	STRCMP_EQUAL( mysqlDatabase.c_str(), mysql.get_database().c_str());
 }
 
 
