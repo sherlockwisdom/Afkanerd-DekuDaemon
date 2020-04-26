270c6612f572ec781edf2162fd1032e66ac1c9e3
Sherlock Wisdom <wisdomnji@gmail.com>
2020-04-26 20:18:04 +0100

    added test for constructor for mysql server

--- UTest/mysql_test.cxx
+++ UTest/mysql_test.cxx
@@ -4,6 +4,22 @@
 #include "CppUTest/TestHarness_c.h"
 #include "CppUTest/CommandLineTestRunner.h"
 
+TEST_GROUP(Mysql) {};
+
+TEST(Mysql, Constructor) {
+	std::string mysqlServer = "localhost";
+	std::string mysqlUser = "root";
+	std::string mysqlPassword = "asshole";
+	std::string mysqlDatabase = "__DEKU__";
+
+	MySQL mysql(mysqlServer, mysqlUser, mysqlPassword, mysqlDatabase);
+
+	STRCMP_EQUAL( mysqlServer.c_str(), mysql->get_server().c_str());
+	STRCMP_EQUAL( mysqlUser.c_str(), mysql->get_user().c_str());
+	STRCMP_EQUAL( mysqlPassword.c_str(), mysql->get_password().c_str());
+	STRCMP_EQUAL( mysqlDatabase.c_str(), mysql->get_database().c_str());
+}
+
 
 int main( int argc, char** argv ) {
 	// Testing to see how the recording works in this aspect
