289787d4a06724961eb7b9d1bf398a84ed3402e0
Sherlock Wisdom <wisdomnji@gmail.com>
2020-04-27 21:23:45 +0100

    added variables

--- UTest/modem_test.cxx
+++ UTest/modem_test.cxx
@@ -6,6 +6,31 @@
 
 TEST_GROUP( Modem ) {};
 
+std::string imei = "";
+std::string isp = "";
+std::string type = "";
+std::string index = "";
+std::map<string,string> configs {};
+MySQL mysqlConnection;
+
+TEST(Modem, constructor) {
+
+}
+
+TEST(Modem, get_failed_counter) {
+}
+
+TEST(Modem, set_exhaust_count) {
+}
+
+TEST(Modem, set_index ) {
+}
+
+TEST(Modem, set_imei ) {
+}
+
+TEST(Modem, set_configs ) {
+}
 
 int main( int argc, char** argv ) {
 	// Testing to see how the recording works in this aspect
