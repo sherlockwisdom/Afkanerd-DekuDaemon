e6f50472e3878315ee2f6a6396b447d0b5081201
Sherlock Wisdom <wisdomnji@gmail.com>
2020-04-29 00:14:18 +0100

    added python parser

---
+++ src/parsers/request_file_parser.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+
+# extracts files
+# applies some custom parsing to it
+# updat files
+
+
+import os
+
+HOME = os.environ['HOME'];
+path = HOME + "/Deku/ISP/MTN";
+print(os.listdir( path ))
