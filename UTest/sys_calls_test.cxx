63f3f6f094816d13fa171e4cd0d86b7c80740769
Sherlock Wisdom <wisdomnji@gmail.com>
2020-04-28 22:58:11 +0100

    tested to see how terminal outputs work

--- UTest/sys_calls_test.cxx
+++ UTest/sys_calls_test.cxx
@@ -6,6 +6,19 @@
 // #include "CppUTest/MockSupport.h"
 
 
+TEST_GROUP(Sys_calls) {};
+
+TEST(Sys_calls, terminal_stdout_void) {
+	map<string,string> ls_output;
+
+	string command = "ls -1 __invalid_file";
+	sys_calls::terminal_stdout(ls_output, command);
+
+	logger::logger(__FUNCTION__, "Return: " + ls_output["return"]);
+	logger::logger(__FUNCTION__, "Data: " + ls_output["data"]);
+}
+
+
 int main( int argc, char** argv ) {
 	// Testing to see how the recording works in this aspect
 	return CommandLineTestRunner::RunAllTests(argc, argv);
