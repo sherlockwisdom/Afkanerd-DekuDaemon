2671c7fe102fd7456ba70e3b56eb28bc363064bc
Sherlock Wisdom <wisdomnji@gmail.com>
2020-04-29 00:46:43 +0100

    does perfect parsing

--- src/parsers/request_file_parser.py
+++ src/parsers/request_file_parser.py
@@ -13,6 +13,11 @@ PATH = HOME + "/Deku/ISP/MTN";
 
 # print(os.listdir( path ))
 
+def custom_parsing( split_message ):
+    del split_message[0]
+
+    return split_message
+
 files = os.listdir( PATH );
 
 for _file in files:
@@ -24,16 +29,25 @@ for _file in files:
     # print( _file );
     # print();
 
-    split_file = _file.split(',')
-    # print(split_file)
-    # print('\n');
+    # split_file = _file.split(',', 1)
+    split_file = _file.split('"')
+    message = split_file[1]
+    split_message = message.split("\\n")
+    
+    split_message = custom_parsing( split_message )
+    print(split_message)
+    print()
 
+    '''
     for split in split_file:
         equal_split = split.split('=');
         if( equal_split[0] == "message" ):
             # print(equal_split[1])
-            message = equal_split[1]
+            message = equal_split[1].replace('"', '')
 
             split_message = message.split("\\n")
+
+            split_message = custom_parsing( split_message )
             print(split_message)
             print()
+    '''
