7be9c04101b97a351ae0d4a03da1a321dabdaa0a
Sherlock Wisdom <wisdomnji@gmail.com>
2020-04-28 22:21:07 +0100

    seeing the command being passed

--- src/sub-routines/modem.cxx
+++ src/sub-routines/modem.cxx
@@ -288,9 +288,12 @@ vector<string> Modem::release_pending_files() {
 
 	map<string,string> ls_returned_values;
 	string pending_string_handle = ".PENDING_" + this->getIMEI() + "_";
-	sys_calls::terminal_stdout(ls_returned_values, "ls -1 " + path_dir_request + "/" + pending_string_handle + "*" );
+	// sys_calls::terminal_stdout(ls_returned_values, "ls -1 " + path_dir_request + "/" + pending_string_handle + "*" );
+	string ls_commands = "ls -1 " + path_dir_request + "/" + pending_string_handle + "*";
+	logger::logger(__FUNCTION__, this->getInfo() + " - " + ls_commands);
 
-	vector<string> filenames = helpers::string_split(ls_returned_values["data"], '\n');
+	string responds = sys_calls::terminal_stdout(ls_commands);
+	vector<string> filenames = helpers::string_split(responds, '\n');
 
 	for( auto file : filenames ) {
 		string source_file = this->getConfigs()["DIR_ISP"] + "/" + this->getISP() + "/" + file;
