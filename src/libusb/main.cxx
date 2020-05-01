3f48cdcefdf1540cf2074dd70579e5c3688f299e
Sherlock Wisdom <wisdomnji@gmail.com>
2020-05-01 17:24:25 +0100

    extracted descriptor information

--- src/libusb/main.cxx
+++ src/libusb/main.cxx
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <libusb-1.0/libusb.h>
 
+using namespace std;
+
 int main(int argc, char** argv) {
 	libusb_device** devices;
 	libusb_context* context = NULL;
@@ -23,36 +25,21 @@ int main(int argc, char** argv) {
 		std::cout << "> Device " << i+1 << "/" << number_of_devices << std::endl;
 		libusb_device* device = devices[i];
 
-		libusb_device_handle* dev_handle;
-		auto open_state = libusb_open(device, &dev_handle);
-		if( open_state != 0) {
-			std::cerr << "Failed to open device " << std::endl;
-			std::cerr << libusb_error_name( open_state ) << std::endl;
+		struct libusb_device_descriptor* device_descriptor;
+
+		//1. Get device descriptor
+		int ret = libusb_get_device_descriptor( device, device_descriptor );
+		if( ret < 0 ) {
+			cerr << errno << endl;
 			continue;
 		}
 
-		/*libusb_device_descriptor dev_desc;
-		if( libusb_get_device_descriptor(device, &dev_desc) < 0) {
-			std::cout << "Failed to get descriptor" << std::endl;
-			continue;
-		}
-
-		//https://github.com/libusb/libusb/blob/master/examples/testlibusb.c
-		int dev_man = dev_desc.bNumConfigurations;
-		*/
-		if( true ) {
-			//std::cout << dev_man << std::endl;
-			unsigned char* data;
-			int device_string_descriptor = libusb_get_string_descriptor_ascii(dev_handle, 0x02, data, 1024);
-			if( device_string_descriptor > 0) {
-				std::cout << "Manufacturer: " << data << std::endl;
-			}
-
-		}
-		std::cout << "DONE" << std::endl;
+		uint8_t num_dev_configs = device_descriptor->bNumConfigurations;
+		cout << "# configs: " << (unsigned int)num_dev_configs << endl;
 		
 		/// Cleaning here
-		libusb_close( dev_handle );
+		//libusb_close( dev_handle );
+		std::cout << "DONE" << std::endl;
 	}
 
 	libusb_free_device_list(devices, 1);
