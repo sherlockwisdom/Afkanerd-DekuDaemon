0fb5da45a354a57ebf4e801a08917c07b92ab03a
Sherlock Wisdom <wisdomnji@gmail.com>
2020-05-10 22:00:57 +0100

    test works as intended

--- UTest/mysql_test.cxx
+++ UTest/mysql_test.cxx
@@ -13,6 +13,9 @@ std::string mysqlUser = "root";
 std::string mysqlPassword = "asshole";
 std::string mysqlDatabase = "CPPUTEST_MYSQL_TEST_DATABASE";
 std::string mysqlTable = "CPPUTEST_MYSQL_TEST_TABLE";
+std::string mysqlTable_columnTypes = "CPPUTEST_MYSQL_TEST_FIELD VARCHAR(255) NOT NULL,\
+				      CPPUTEST_MYSQL_TEST_FIELD1 INT NOT NULL AUTO_INCREMENT,\
+				      CPPUTEST_MYSQL_TEST_FIELD2 DATE";
 
 TEST_GROUP(Mysql) {};
 TEST_GROUP(Mysql_integration) {};
@@ -109,18 +112,23 @@ TEST(Mysql_integration, create_table) {
 	MySQL mysql(mysqlServer, mysqlUser, mysqlPassword);
 	CHECK( mysql.connect() );
 
-	if( mysql.has_database( mysqlDatabase ) )
-		mysql.delete_database( mysqlDatabase );
-	bool create_database_state = mysql.create_database( mysqlDatabase );
-	bool has_database = mysql.has_database( mysqlDatabase );
-	CHECK( create_database_state == true and has_database == true );
+	if( !mysql.has_database( mysqlDatabase ) ) {
+		bool create_database_state = mysql.create_database( mysqlDatabase );
+		bool has_database = mysql.has_database( mysqlDatabase );
+		CHECK( create_database_state == true and has_database == true );
+	}
 
 	bool has_table = mysql.has_table( mysqlTable );
-	CHECK( has_table == true );
+	if( has_table ) 
+		mysql.delete_table( mysqlTable );
 
-	bool delete_database_state = mysql.delete_database( mysqlDatabase );
-	has_database = mysql.has_database( mysqlDatabase );
-	CHECK( delete_database_state == true and has_database == false );
+	bool create_table = mysql.create_table( mysqlTable, mysqlTable_columnTypes );
+	has_table = mysql.has_table( mysqlTable );
+	CHECK( create_table == true and has_table == false );
+
+	bool delete_table_state = mysql.delete_table( mysqlTable );
+	has_table = mysql.has_table( mysqlTable );
+	CHECK( delete_table_state == true and has_table == false );
 }
 
 TEST(Mysql_integration, delete_table) {
@@ -135,7 +143,7 @@ TEST(Mysql_integration, delete_table) {
 
 	bool has_table = mysql.has_table( mysqlTable );
 	if( !has_table ) {
-		bool create_table = mysql.create_table( mysqlTable );
+		bool create_table = mysql.create_table( mysqlTable, mysqlTable_columnTypes );
 		CHECK( has_table == false and create_table == true );
 	}
 
