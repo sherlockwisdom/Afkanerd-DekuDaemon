c8b1084b82d2ad9b3514345992b09c089a46ba5e
Sherlock Wisdom <wisdomnji@gmail.com>
2020-05-12 08:39:18 +0100

    refactored all of them

---
+++ src/formatters/logger.cxx
@@ -0,0 +1,40 @@
+#include "logger.hpp"
+using namespace std;
+
+namespace logger {
+
+	string show_state = "TESTING";
+	void logger( const char* func_name, string output, string output_stream = "stdout", bool show_production = false) {
+		if( show_state == "PRODUCTION" and !show_production) return;
+		if( output.empty() ) return;
+
+		if( output_stream == "stdout" || output_stream == "STDOUT" ) {
+			cout << "[logger.info] - " << func_name << "=> " << output << endl;
+		}
+		else if( output_stream == "stderr" || output_stream == "STDERR" ) {
+			cerr << "[logger.error] - " << func_name << "=> " << output << endl;
+		}
+
+		else cerr << "[logger.error] - LOGGER DOESN'T HAVE THAT STATE YET" << endl;
+	}
+
+
+	void logger_errno( int t_errno ) {
+		char str_error[256];
+		string error_message = strerror_r( t_errno, str_error, 256);
+		cout << "[logger_errno] - ERRNO: " << t_errno << endl;
+		cout << "[logger_errno] - MESSAGE: " << error_message << "=> " << endl;
+	}
+
+	void logger_tester( string func_name, bool output, string output_stream = "stdout" ) {
+		if( output_stream == "stdout" || output_stream == "STDOUT" ) {
+			cout << "[tester.info] - " << func_name << "=> " << std::boolalpha << output << endl;
+		}
+		else if( output_stream == "stderr" || output_stream == "STDERR" ) {
+			cerr << "[tester.error] - " << func_name << "=> " << std::boolalpha << output << endl;
+		}
+
+		else cerr << "[tester.error] - LOGGER DOESN'T HAVE THAT STATE YET" << endl;
+	}
+}
+
