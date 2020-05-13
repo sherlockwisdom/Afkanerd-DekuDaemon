11fb88781ae58f605321672f708768c4bd7b1064
Sherlock Wisdom <wisdomnji@gmail.com>
2020-05-13 22:29:59 +0100

    script update, but cannot remove first character from being only .

--- scripts/modem_information_extraction.sh
+++ scripts/modem_information_extraction.sh
@@ -50,8 +50,8 @@ elif [ "$1" == "ussd_cancel" ]; then
 
 elif [ "$1" == "list_locked_request_files" ]; then
 	locked_dir=$2
-	files=$( ls -a | grep -oe "^\.\w*$" )
-	printf "files"
+	files=$( ls -a $locked_dir | grep -oe "^\.\w*$" )
+	printf "$files"
 
 elif [ "$1" == "sms" ] ; then
 	_type=$2
