9c71f1ced3beeec1568cee5c063889c818c373c9
Sherlock Wisdom <wisdomnji@gmail.com>
2020-05-16 18:28:51 +0100

    using native C functions in making MySQL work

--- src/mysql/mysql.cxx
+++ src/mysql/mysql.cxx
@@ -23,7 +23,7 @@ string MySQL::escape_string( const char* query_string ) {
 	char to[strlen(query_string)*2 + 1];
 
 	//int length = mysql_real_escape_string(this->mysqlConnection, to, query_string, strlen(query_string));
-	int length = mysql_real_escape_string(this->mysqlConnection, to, "Hello world", 13);
+	int length = mysql_real_escape_string(this->mysqlConnection, to, query_string, strlen(query_string));
 	if( length == -1 ) {
 		logger::logger(__FUNCTION__, "ESCAPING STRING ERROR", "stdout", true);
 		logger::logger(__FUNCTION__, mysql_error( this->mysqlConnection ), "stderr");
