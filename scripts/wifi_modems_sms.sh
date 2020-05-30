5f334b5196040a61707536ae331bfef545b91f20
nSedrickm <nsedrick101@gmail.com>
2020-05-30 16:34:46 +0100

    no need to write token and session to external file but still fails to send

--- scripts/wifi_modems_sms.sh
+++ scripts/wifi_modems_sms.sh
@@ -4,26 +4,14 @@
 # We found using mmcli wont work for Huawei E5573s-XXX wifi modems . A possible solution would be to use usb_modeswitch
 # to make the modem work like a normal usb key
 
-#get session token
-curl -b session.txt -c session.txt http://192.168.8.1/html/index.html > /dev/null 2>&1
+# This looks interesting : https://www.mrt-prodz.com/blog/view/2015/05/huawei-modem-api-and-data-plan-monitor
+# Explains the error codes nicely https://godoc.org/github.com/knq/hilink
+# https://blog.idorobots.org/entries/hacking-huawei-e3372-hilink..html
 
-#get token
-TOKEN=$(curl -s -b session.txt -c session.txt http://192.168.8.1/html/smsinbox.html)
-TOKEN=$(echo $TOKEN | cut -d'"' -f 10)
+#!/bin/bash
 
-echo $TOKEN > token.txt
+DATA=$(curl http://192.168.8.1/api/webserver/SesTokInfo)
+SESSION_ID=$(echo "$DATA" | grep "SessionID=" | cut -b 10-147)
+TOKEN=$(echo "$DATA" | grep "TokInfo" | cut -b 10-41)
 
-#send sms
-
-NUMBER=670471866
-MESSAGE=hello
-
-LENGTH=${#MESSAGE}
-TIME=$(date +"%Y-%m-%d %T")
-TOKEN=$(<token.txt)
-
-SMS="<request><Index>-1</Index><Phones><Phone>$NUMBER</Phone></Phones><Sca/><Content>$MESSAGE</Content><Length>$LENGTH</Length><Reserved>1</Reserved><Date>$TIME</Date></request>"
-
-echo $SMS
-
-curl -v -b session.txt -c session.txt -H "X-Requested-With: XMLHttpRequest" --data "$SMS" http://192.168.8.1/api/sms/send-sms --header "__RequestVerificationToken: $TOKEN" --header "Content-Type:text/xml"
+curl http://192.168.8.1/api/sms/send-sms -H "Cookie: $SESSION_ID" -H "__RequestVerificationToken: $TOKEN" --data "<?xml version='1.0' encoding='UTF-8'?><request><Index>-1</Index><Phones><Phone>$1</Phone></Phones><Sca></Sca><Content>$2</Content><Length>-1</Length><Reserved>1</Reserved><Date>-1</Date></request>"
