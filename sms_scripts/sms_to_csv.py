e68c3f3992a5348b788e86a3eab93a7d5b53875a
Idadelveloper <idadelm@gmail.com>
2020-08-06 01:42:40 -0700

    enables input of command line argument to set the csv filename

--- sms_scripts/sms_to_csv.py
+++ sms_scripts/sms_to_csv.py
@@ -1,5 +1,11 @@
-# After importing the database into your MySQL database,
+import os
+import argparse
 
+parser = argparse.ArgumentParser(description='to csv file')
+parser.add_argument("-f", "--filename", help="File name")
+args = parser.parse_args()
+
+# After importing the database into your MySQL database,
 from sqlalchemy import create_engine
 import pymysql
 import pandas as pd
@@ -10,5 +16,5 @@ db_connection = create_engine(db_connection_str)
 df = pd.read_sql('SELECT * FROM MODEM_SMS_RECEIVED', con=db_connection)
 print(df)
 
-#converts datataframe to csv and stores in file called 'modem_sms_received.csv'
-df.to_csv(r"path_to_storage_file\modem_sms_received.csv", index = False, header=True)
+# converts datataframe to csv and stores in file inputed from the command line as argument
+df.to_csv(os.path.join(r"path_to_storage_file", str(args.filename)), index = False, header=True)
