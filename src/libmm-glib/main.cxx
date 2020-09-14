e85c0a872f7195a185fb75f23984c2b2e2215182
Sherlock Wisdom <wisdomnji@gmail.com>
2020-09-14 22:53:48 +0100

    updated the structure of the makefile to allow for compilation

--- src/libmm-glib/main.cxx
+++ src/libmm-glib/main.cxx
@@ -2,5 +2,14 @@
 
 #include <libmm-glib.h>
 
+using namespace std;
+
 int main() {
+	MMObject* mmobject;
+
+	auto mm_path = mm_object_get_path( mmobject );
+
+	cout << mm_path << endl;
+
+	return 0;
 }
