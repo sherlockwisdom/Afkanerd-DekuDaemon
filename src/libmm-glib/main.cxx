b4c94e41e1657662b57a8b8fbd6e84349724ae30
Sherlock Wisdom <wisdomnji@gmail.com>
2020-09-14 23:34:34 +0100

    scanning with default modemmanager now

--- src/libmm-glib/main.cxx
+++ src/libmm-glib/main.cxx
@@ -4,12 +4,28 @@
 
 using namespace std;
 
+void init_callback(auto source_object, auto res, auto user_data ) {
+	cout << "In callback..." << endl;
+
+	GError **error;
+	MMManager* mm_manager = mm_manager_new_finish( res, error);
+
+	// mm_manager_scan_devices( mm_manager, cancellable, callback, user_data);
+}
+
 int main() {
-	MMObject* mmobject;
 
-	auto mm_path = mm_object_get_path( mmobject );
+	GDBusConnection *connection;
+	GDBusObjectManagerClientFlags flags;
+	GCancellable *cancellable;
+	// GAsyncReadyCallback callback = init_callback;
+	gpointer user_data;
+	GError** error;
 
-	cout << mm_path << endl;
 
+	MMManager* mm_manager = mm_manager_new_sync( connection, flags, cancellable, error);
+	gboolean scan_state = mm_manager_scan_devices_sync( mm_manager, cancellable, error );
+
+	cout << "scan_state: " << scan_state << endl;
 	return 0;
 }
