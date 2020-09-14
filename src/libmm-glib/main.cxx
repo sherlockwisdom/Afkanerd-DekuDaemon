70b98e969d1f7c035ef8569d25c4453cbe740725
Sherlock Wisdom <wisdomnji@gmail.com>
2020-09-15 00:08:14 +0100

    everything initialized with no errors

--- src/libmm-glib/main.cxx
+++ src/libmm-glib/main.cxx
@@ -15,16 +15,22 @@ void init_callback(auto source_object, auto res, auto user_data ) {
 
 int main() {
 
-	GDBusConnection *connection;
-	GDBusObjectManagerClientFlags flags;
-	GCancellable *cancellable;
-	// GAsyncReadyCallback callback = init_callback;
-	gpointer user_data;
-	GError** error;
+	GError* error;
+	GBusType bus_type = G_BUS_TYPE_STARTER;
+	GCancellable* cancellable = g_cancellable_new( );
+	GDBusConnection* connection = g_bus_get_sync( bus_type, cancellable, &error);
+	if ( error != NULL ) {
+		cerr << error->message << endl;
+		g_clear_error( &error );
+	}
+	GDBusObjectManagerClientFlags flags = G_DBUS_OBJECT_MANAGER_CLIENT_FLAGS_NONE;
 
-
-	MMManager* mm_manager = mm_manager_new_sync( connection, flags, cancellable, error);
-	gboolean scan_state = mm_manager_scan_devices_sync( mm_manager, cancellable, error );
+	MMManager* mm_manager = mm_manager_new_sync( connection, flags, NULL, &error);
+	if ( error != NULL ) {
+		cerr << error->message << endl;
+		g_clear_error( &error );
+	}
+	gboolean scan_state = mm_manager_scan_devices_sync( mm_manager, NULL, &error );
 
 	cout << "scan_state: " << scan_state << endl;
 	return 0;
