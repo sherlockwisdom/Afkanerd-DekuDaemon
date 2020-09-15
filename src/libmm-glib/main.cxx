75d0751c9e751c19f54772ba650ae44207ce6166
Sherlock Wisdom <wisdomnji@gmail.com>
2020-09-15 12:14:07 +0100

    using old connection to make changes

--- src/libmm-glib/main.cxx
+++ src/libmm-glib/main.cxx
@@ -16,30 +16,41 @@ void init_callback(auto source_object, auto res, auto user_data ) {
 
 int main() {
 
-	/*
+	// /*
 	GError* error;
 	GBusType bus_type = G_BUS_TYPE_SYSTEM;
 	GCancellable* cancellable = g_cancellable_new( );
-	GDBusConnection* connection = g_bus_get_sync( bus_type, cancellable, &error);
-	if ( error != NULL ) {
+
+	GIOStream* stream;
+	const gchar* guid = NULL;
+	GDBusConnectionFlags flags = G_DBUS_CONNECTION_FLAGS_NONE;
+	GDBusAuthObserver *observer = NULL;
+	GDBusConnection* connection = g_dbus_connection_new_sync( stream, guid, flags, observer, cancellable, &error);
+	if ( error  ) {
 		cerr << "Error getting bus sync..." << endl;
 		cerr << error->message << endl;
 		g_clear_error( &error );
+		return 1;
 	}
-	GDBusObjectManagerClientFlags flags = G_DBUS_OBJECT_MANAGER_CLIENT_FLAGS_NONE;
+	else {
+		cout << "GDBusConnection acquired..." << endl;
+	}
+	GDBusObjectManagerClientFlags gd_object_flags = G_DBUS_OBJECT_MANAGER_CLIENT_FLAGS_NONE;
 	// GDBusObjectManagerClientFlags flags = G_DBUS_OBJECT_MANAGER_CLIENT_FLAGS_DO_NOT_AUTO_START;
-	*/
+	// */
 
-	/*
-	MMManager* mm_manager = mm_manager_new_sync( connection, flags, NULL, &error);
+	// /*
+	MMManager* mm_manager = mm_manager_new_sync( connection, gd_object_flags, NULL, &error);
 	if ( error != NULL ) {
 		cerr << error->message << endl;
 		g_clear_error( &error );
 		return 1;
 	}
-	// auto mm_version = mm_manager_get_version( mm_manager );
-	// cout << "Version: " << mm_manager_get_version( mm_manager ) << endl;
+	// auto GD_Bus_Proxy = mm_manager_peek_proxy( mm_manager );
+	
+	// cout << "Version: " << mm_manager_version << endl;
 	gboolean scan_state = mm_manager_scan_devices_sync( mm_manager, NULL, &error );
+	const gchar* mm_manager_version = mm_manager_get_version( mm_manager );
 
 	if( scan_state ) {
 		cout << "Scan successful...." << endl;
@@ -48,10 +59,7 @@ int main() {
 		cout << "Scan failed..." << endl;
 		cout << error->message << endl;
 	}
-	*/
-
-	MMObject* mm_object;
-	// MMModem* modem = mm_object_get_modem( mm_object );
+	// */
 
 	return 0;
 }
